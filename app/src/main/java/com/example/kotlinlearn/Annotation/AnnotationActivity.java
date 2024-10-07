54420568a23b60f34edebd410deead4b28e7b19a
Yanghiqi <1973182107@qq.com>
2024-10-07 15:17:35 +0800

    [] first commit

--- app/src/main/java/com/example/kotlinlearn/Annotation/AnnotationActivity.java
+++ app/src/main/java/com/example/kotlinlearn/Annotation/AnnotationActivity.java
@@ -9,6 +9,7 @@ package com.example.kotlinlearn.Annotation;
 //}
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,6 @@ public class AnnotationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("TAG", "onCreate: ");
     }
 }
